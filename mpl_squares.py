a0ffd15dd2a0e973a8c774821ad41da317fee364
readyshen <readyshen@gmail.com>
2024-06-12 16:08:57 +0800

    设置标题和轴标签、刻度样式

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
 
+# 设置标题和轴标签
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# 设置刻度样式
+ax.tick_params(labelsize=14)
 plt.show()
