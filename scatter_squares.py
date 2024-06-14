31cbd30b47e24578d9948d2d42bd5b8e713c4025
readyshen <readyshen@gmail.com>
2024-06-14 19:14:32 +0800

    画出于y值相关联的颜色渐变,且同时画出两条曲线

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,9 @@ fig, ax = plt.subplots()
 
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
-ax.scatter(x_values, y_values, s=0.6)
+y2_values = [y*2 for y in y_values ]
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=0.6)
+ax.scatter(x_values, y2_values, c=y_values, cmap=plt.cm.Reds, s=0.6)
 
 ax.set_title("Scatter Square Numbers")
 ax.set_xlabel("Value", fontsize=14)
