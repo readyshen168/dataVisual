315394636dbadcbe0203fbe5e611d4cdccada256
readyshen <readyshen@gmail.com>
2024-06-14 18:46:18 +0800

    指定图表样式

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4]
 squares = [1, 4, 9, 16]
 
+# 指定图标样式
+plt.style.use('dark_background')
+
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
