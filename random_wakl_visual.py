bec04c9ba56f1ae0ed23e192583f1501331db7a1
readyshen <readyshen@gmail.com>
2024-06-16 22:46:53 +0800

    更改游走数量，观察图形变化

--- random_wakl_visual.py
+++ random_wakl_visual.py
@@ -7,7 +7,9 @@ fig, ax = plt.subplots()
 # 新建随机游走类
 rw = RandomWalk()
 
-ax.scatter(rw.x_values, rw.y_values, s=1)
+point_numbers = range(rw.num_points)
+
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
 # 让两条坐标轴上的刻度间距相等
 ax.set_aspect('equal')
