27cf0fce946695d4dbad81dfed08363a4e66b89f
readyshen <readyshen@gmail.com>
2024-06-17 12:57:39 +0800

    隐藏坐标轴

--- random_wakl_visual.py
+++ random_wakl_visual.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-plt.style.use('dark_background')
+plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
 
 # 新建随机游走类
@@ -13,7 +13,10 @@ ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecol
 # 标红第一个点
 ax.scatter(rw.y_values[0], rw.x_values[0], c='red', edgecolors='none', s=50)
 # 标绿最后一个点
-ax.scatter(rw.y_values[-1], rw.x_values[-1], c='green', edgecolors='none', s=50)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='green', edgecolors='none', s=50)
+# 隐藏坐标轴
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
 
 # 让两条坐标轴上的刻度间距相等
 ax.set_aspect('equal')
