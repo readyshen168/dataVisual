677b72e5ff2875f8316228d4332f5dafc6539f7c
readyshen <readyshen@gmail.com>
2024-06-17 12:51:51 +0800

    给第一个点和最后一个点标出不同颜色

--- random_wakl_visual.py
+++ random_wakl_visual.py
@@ -10,6 +10,10 @@ rw = RandomWalk()
 point_numbers = range(rw.num_points)
 
 ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+# 标红第一个点
+ax.scatter(rw.y_values[0], rw.x_values[0], c='red', edgecolors='none', s=50)
+# 标绿最后一个点
+ax.scatter(rw.y_values[-1], rw.x_values[-1], c='green', edgecolors='none', s=50)
 
 # 让两条坐标轴上的刻度间距相等
 ax.set_aspect('equal')
