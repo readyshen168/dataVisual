630f265e1b902e864e8220fe42b6d36cee392957
readyshen <readyshen@gmail.com>
2024-06-19 23:34:33 +0800

    优化fill_walk方法

--- random_walk.py
+++ random_walk.py
@@ -21,20 +21,31 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # 随机决定前进的方向以及距离
-            x_direction = choice([-1,1])
-            x_distance = choice([0,1,2,3,4,5])
+            """
+            x_direction = choice([-1, 1])
+            x_distance = choice([0, 1, 2, 3, 4, 5])
             x_step = x_distance * x_direction
 
-            y_direction = choice([-1,1])
-            y_distance = choice([0,1,2,3,4,5])
+            y_direction = choice([-1, 1])
+            y_distance = choice([0, 1, 2, 3, 4, 5])
             y_step = y_distance * y_direction
+            """
+            # 得到坐标的随机步数
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # 禁止原地踏步
             if x_step == 0 and y_step == 0:
                 continue
 
             # 加入新的游走坐标
-            self.x_values.append(self.x_values[-1]+x_step)
-            self.y_values.append(self.y_values[-1]+y_step)
+            self.x_values.append(self.x_values[-1] + x_step)
+            self.y_values.append(self.y_values[-1] + y_step)
 
+    def get_step(self):
+        """得到随机步数"""
+        direction = choice([-1, 1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        step = distance * direction
 
+        return step
